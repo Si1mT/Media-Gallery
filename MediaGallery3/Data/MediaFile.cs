85c78c227a3a2a92dc230bb69d72258e6dae5a71
Si1mT <33309403+Si1mT@users.noreply.github.com>
2018-09-14 15:16:54 +0300

    first commit

---
+++ MediaGallery3/Data/MediaFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaGallery3.Data
+{
+    public class MediaFile :MediaItem
+    {
+        public string FileName { get; set; }
+    }
+}
