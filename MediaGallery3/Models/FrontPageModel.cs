e027ac05c958a873d8d849e580c263b44b971a5e
Si1mT <33309403+Si1mT@users.noreply.github.com>
2018-09-14 15:36:45 +0300

    FrontPageModel class

---
+++ MediaGallery3/Models/FrontPageModel.cs
@@ -0,0 +1,14 @@
+﻿using MediaGallery3.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaGallery3.Models
+{
+    public class FrontPageModel
+    {
+        public IList<MediaItem> NewPhotos { get; set; }
+        public IList<MediaItem> PopularPhotos { get; set; }
+    }
+}
